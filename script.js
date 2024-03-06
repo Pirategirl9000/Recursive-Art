f9b84c466c304257bdc9321f4a2cf2a872f06e49
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-03-06 21:15:58 +0000

    I don't know what changed but it looks cool

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect();
 
-function drawCell(x, y, d) {
+function drawTriangle(x, y, d) {
+    if (y >= canvas.height) {return 1;}
+    if (x >= canvas.width) {return drawTriangle(0, y + d, d/1.07);}
     //x is x-location
     //y is y-location
     //d is the width and height because it's a square
     ctx.beginPath();
     ctx.fillStyle = "blue";
-    ctx.fillRect(x, y, d);
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + d*2, y);
+    ctx.lineTo(x + d, y + d);
+    ctx.lineTo(x, y);
     ctx.fill();
     ctx.closePath();
+
+    return drawTriangle(x + d*2, y, d);
 }
 
-drawCell(0, 0, 20);
+drawTriangle(0, 0, 12);
